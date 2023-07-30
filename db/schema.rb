3f3826339f167df5a3fb34facbf781963f6a593d
soikawa <soikawa0108@gmail.com>
2023-07-31 02:54:13 +0900

    Revert "Revert "LINElogin""
    This reverts commit d01e4c871bcfb69f28e6acff988b02268fb433c3.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_27_143702) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_29_142959) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -74,6 +74,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_27_143702) do
     t.integer "role"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "line_user_id", null: false
   end
 
   add_foreign_key "albums", "idols"
