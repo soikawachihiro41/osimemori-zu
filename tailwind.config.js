63f243ba2c41e135799cacac53c5600812bc2ed9
soikawa <soikawa0108@gmail.com>
2023-08-07 21:04:07 +0900

    top page

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,22 @@ module.exports = {
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
   ],
-  plugins: [
-    require("daisyui"),
-  ],
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#5c7b64",
+          "primary-content": "#ffe4e6",
+          "base-100": "#fff1f2",
+          "base-content": "#372f38",
+          extend: {
+            fontFamily: {
+              'zenmaru': ['Zen Maru Gothic', 'serif'],
+            },
+          },
+        },
+      }
+    ],
+  },
 }
